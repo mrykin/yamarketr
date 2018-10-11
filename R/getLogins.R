87a710dea44826dcad7cb79378a5a9bf6c3cf955
Pavel Mrykin <mrykin.pavel@gmail.com>
2018-10-11 16:24:52 +0300

    add token and client_id params

--- R/getLogins.R
+++ R/getLogins.R
@@ -1,12 +1,18 @@
-getLogins <- function(shops, howmuch = NULL){
+# Получаем логины магазинов
+getLogins <- function(shops, howmuch = NULL, token = NULL, client_id = NULL){
+  if (is.null(token)) {
+    stop("Введите свой API-токен")
+  } else if (is.null(client_id)){
+    stop("Введите client_id вашего приложения")
+  }
   result <- data.frame(id = character(0), logins = character(0))
 
   for(i in 1:nrow(shops)){
     query <- paste0("https://api.partner.market.yandex.ru/v2/campaigns/",shops$id[i],"/logins.json")
     raw <- httr::GET(url=query,
                      httr::add_headers(Authorization=paste0("OAuth oauth_token=",
-                                                            yamtoken,",oauth_client_id=",
-                                                            yamclient_id)))
+                                                            token,",oauth_client_id=",
+                                                            client_id)))
     data <- jsonlite::fromJSON(httr::content(raw, type="text", encoding = "UTF-8"), flatten = TRUE)
     result <- rbind(result, data.frame(id = shops$id[i],
                                        logins = ifelse(is.null(howmuch),
