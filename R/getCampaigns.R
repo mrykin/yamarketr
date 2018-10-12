4bd9565897bd0093931f66d6066681511ed97552
Mrykin Pavel <mrykin@mediaguru.ru>
2018-10-13 02:04:23 +0300

    Update getCampaigns.R

--- R/getCampaigns.R
+++ R/getCampaigns.R
@@ -1,10 +1,6 @@
 # Получаем список магазинов
 getCampaigns <- function(Token = NULL, client_id = "8943390a15784189a8538ce5c4d57dfb", Login = NULL, TokenPath = getwd()){
-  if (is.null(Token)) {
-    stop("Введите свой API-токен")
-  } else if (is.null(client_id)){
-    stop("Введите client_id вашего приложения")
-  }
+
   #Авторизация
   Token <- tech_auth(login = Login, token = Token, TokenPath = TokenPath)
   result <- data.frame(id = character(0), domain = character(0), state = numeric(0), stateReasons = numeric(0))
