4d907e0482f3f216639f13a6a4d85d7d54745fa9
Mrykin Pavel <mrykin@mediaguru.ru>
2018-10-30 17:15:48 +0300

    Update yamarketrGetBalance.R

--- R/yamarketrGetBalance.R
+++ R/yamarketrGetBalance.R
@@ -9,13 +9,14 @@ yamarketrGetBalance <- function(Campaigns = NULL, Login = NULL, TokenPath = getw
   result <- data.frame(id = character(0),
                        balance = numeric(0), daysLeft = integer(0), recomendedPayment = numeric(0),
                        stringsAsFactors = FALSE)
+  pb   <- txtProgressBar(1, nrow(Campaigns), style=3)
   for(i in 1:nrow(Campaigns)){
     campaignId <- ifelse(is.vector(Campaigns), Campaigns[i], Campaigns$id[i])
     query <- paste0("https://api.partner.market.yandex.ru/v2/campaigns/", campaignId, "/balance.json")
     raw <- httr::GET(url=query, httr::add_headers(Authorization=paste("OAuth oauth_token=",Token,",oauth_client_id=8943390a15784189a8538ce5c4d57dfb")))
     data <- jsonlite::fromJSON(httr::content(raw,type="text", encoding = "UTF-8"), flatten = TRUE)
     if(raw$status_code > 200){
-      stop(paste(data$errors$code, "-", data$errors$message))
+      stop(paste(data$errors$code, "-", data$errors$message, "-", Campaigns$id[i]))
     }
     if(is.null(data$balance)) next
     result <- rbind(result, data.frame(id = as.character(campaignId),
@@ -24,7 +25,8 @@ yamarketrGetBalance <- function(Campaigns = NULL, Login = NULL, TokenPath = getw
                                        recommendedPayment = data$balance$recommendedPayment,
                                        stringsAsFactors = FALSE)
     )
-    message(paste("Получены данные:", campaignId, "-", Campaigns$domain[i]))
+    setTxtProgressBar(pb, i)
   }
+  message(paste("\nЗагрузка завершена!"))
   return(result)
 }
