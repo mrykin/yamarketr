80d1b95e7cf0653e4e7288b7187629035dff1fe4
Mrykin Pavel <mrykin@mediaguru.ru>
2018-11-01 16:31:26 +0300

    Add vectors for variable Campaigns

--- R/yamarketrGetBalance.R
+++ R/yamarketrGetBalance.R
@@ -9,8 +9,9 @@ yamarketrGetBalance <- function(Campaigns = NULL, Login = NULL, TokenPath = getw
   result <- data.frame(id = character(0),
                        balance = numeric(0), daysLeft = integer(0), recomendedPayment = numeric(0),
                        stringsAsFactors = FALSE)
-  pb   <- txtProgressBar(1, nrow(Campaigns), style=3)
-  for(i in 1:nrow(Campaigns)){
+  nrowCampaigns <- ifelse(is.vector(Campaigns), length(Campaigns), nrow(Campaigns))
+  pb   <- txtProgressBar(1, nrowCampaigns, style=3)
+  for(i in 1:nrowCampaigns){
     campaignId <- ifelse(is.vector(Campaigns), Campaigns[i], Campaigns$id[i])
     query <- paste0("https://api.partner.market.yandex.ru/v2/campaigns/", campaignId, "/balance.json")
     raw <- httr::GET(url=query, httr::add_headers(Authorization=paste("OAuth oauth_token=",Token,",oauth_client_id=8943390a15784189a8538ce5c4d57dfb")))
