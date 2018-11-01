abd75c41bb645f402db66df6a1e44772c825e128
Mrykin Pavel <mrykin@mediaguru.ru>
2018-11-01 16:52:06 +0300

    Update yamarketrGetLogins.R

--- R/yamarketrGetLogins.R
+++ R/yamarketrGetLogins.R
@@ -17,9 +17,9 @@ yamarketrGetLogins <- function(Campaigns, howmuch = NULL,
                                                             Token,",oauth_client_id=8943390a15784189a8538ce5c4d57dfb")))
     data <- jsonlite::fromJSON(httr::content(raw, type="text", encoding = "UTF-8"), flatten = TRUE)
     if(raw$status_code > 200){
-      stop(paste(data$errors$code, "-", data$errors$message, "-", Campaigns$id[i]))
+      stop(paste(data$errors$code, "-", data$errors$message, "-", campaignId))
     }
-    result <- rbind(result, data.frame(id = as.character(Campaigns$id[i]),
+    result <- rbind(result, data.frame(id = as.character(campaignId),
                                        logins = ifelse(is.null(howmuch),
                                                        paste(data$logins, collapse = ', '),
                                                        data$logins[1:howmuch]),
