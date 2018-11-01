cfdc4cb9a6da2bc1a2ebab635cd5fbc1fa78e71e
Mrykin Pavel <mrykin@mediaguru.ru>
2018-11-01 16:51:46 +0300

    Update yamarketrGetBalance.R

--- R/yamarketrGetBalance.R
+++ R/yamarketrGetBalance.R
@@ -17,7 +17,7 @@ yamarketrGetBalance <- function(Campaigns = NULL, Login = NULL, TokenPath = getw
     raw <- httr::GET(url=query, httr::add_headers(Authorization=paste("OAuth oauth_token=",Token,",oauth_client_id=8943390a15784189a8538ce5c4d57dfb")))
     data <- jsonlite::fromJSON(httr::content(raw,type="text", encoding = "UTF-8"), flatten = TRUE)
     if(raw$status_code > 200){
-      stop(paste(data$errors$code, "-", data$errors$message, "-", Campaigns$id[i]))
+      stop(paste(data$errors$code, "-", data$errors$message, "-", campaignId))
     }
     if(is.null(data$balance)) next
     result <- rbind(result, data.frame(id = as.character(campaignId),
